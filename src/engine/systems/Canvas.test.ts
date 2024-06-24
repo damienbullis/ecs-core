3c5d1ec6fca4bfdf612ae2c59554e0223117d572
Damien <80228708+damienbullis@users.noreply.github.com>
2024-06-23 22:08:50 -0500

    feat: Add Canvas test for creating and resizing canvas element

---
+++ src/engine/systems/Canvas.test.ts
@@ -0,0 +1,35 @@
+import { describe, test, expect, jest } from 'bun:test';
+import { Canvas } from './';
+import { ECSCore } from '../core';
+
+// polyfill document and window
+global.document = {
+	//@ts-expect-error - jest.fn() is a mock function
+	createElement: jest.fn(() => ({
+		getContext: jest.fn(() => ({})),
+	})),
+	//@ts-expect-error - jest.fn() is a mock function
+	body: {
+		appendChild: jest.fn(),
+	},
+};
+
+const addEventListener = jest.fn();
+
+//@ts-expect-error - jest.fn() is a mock function
+global.window = {
+	addEventListener,
+};
+
+describe('Canvas', () => {
+	const ecs = new ECSCore();
+	ecs.addSystem(new Canvas(ecs));
+	test('Creates a canvas element', () => {
+		expect(document.createElement).toHaveBeenCalledWith('canvas');
+		expect(document.body.appendChild).toHaveBeenCalled();
+	});
+
+	test('Resizes the canvas on mount', () => {
+		expect(addEventListener).toHaveBeenCalled();
+	});
+});
