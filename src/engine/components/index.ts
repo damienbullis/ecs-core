42c2710cc8b8b8fbe9e9f43a6eb0065a547b1e88
Damien <80228708+damienbullis@users.noreply.github.com>
2024-06-23 22:08:37 -0500

    feat: Add new components for game entities

---
+++ src/engine/components/index.ts
@@ -0,0 +1,42 @@
+import { Component } from '../core';
+
+export class TurnCount implements Component {
+	turnCount = 0;
+}
+
+export class PlayerEntityState implements Component {
+	id = 0;
+	teamId = 0;
+	location = 0; // spaceId
+	x = 0;
+	y = 0;
+	width = 0;
+	height = 0;
+	timerMax = 30;
+	actionQueued = false;
+	constructor({ teamId, id }: { teamId: number; id: number }) {
+		this.teamId = teamId;
+		this.id = id;
+	}
+}
+
+export class SpaceState implements Component {
+	id = 0;
+	location = 0;
+	entities: number[] = [];
+	x: number = 0;
+	y: number = 0;
+	width: number = 0;
+	height: number = 0;
+	status: string[] = [];
+	occupied: boolean = false;
+}
+
+export class CanvasComponent implements Component {
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D;
+	constructor(canvas: HTMLCanvasElement) {
+		this.canvas = canvas;
+		this.ctx = canvas.getContext('2d')!;
+	}
+}
